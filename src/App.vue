03b486879020f3d9bd3c654b878842edd8cedf30
soyUnaGata <fallen.snitch@gmail.com>
2023-04-16 11:22:24 +0300

    add style for header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav> -->
+<div class="container">
+  <header class="header d-flex justify-content-between align-items-center">
+    <h3 class="headline">a.tiempo</h3>
+    <span class="add-new-task">+</span>
+  </header>
+
+  <main class="main">
+    <div class="schedule margin-top-2-5rem d-flex align-items-center justify-content-center">
+      <button type="button" class="main-btn calendar">Schedule</button>
+      <button type="button" class="main-btn today">Today</button>
+    </div>
+
+    <div class="wrapper">
+      <div class="current-calendar">
+        <div>
+          <h2 class="current-date">April 2022</h2>
+          
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+</div>
+
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +50,8 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
+</style>
+
+<style>
 </style>
