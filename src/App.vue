3f3f8281a7c6ca76f427c5e513c5565190da584b
soyUnaGata <fallen.snitch@gmail.com>
2023-04-16 12:39:28 +0300

    add and style calendar

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,65 @@
       <button type="button" class="main-btn today">Today</button>
     </div>
 
-    <div class="wrapper">
+    <div class="wrapper margin-top-2rem">
       <div class="current-calendar">
-        <div>
+        <div class="headline-calendar d-flex justify-content-between align-items-center">
           <h2 class="current-date">April 2022</h2>
-          
+          <div class="icons d-flex gap-5px">
+            <span class="material-symbols-outlined">chevron_left</span>
+            <span class="material-symbols-outlined">chevron_right</span>
+          </div>
+        </div>
+
+        <div class="main-calendar d-flex flex-wrap-wrap margin-top-1rem">
+          <ul class="weeks d-flex w-100">
+            <li>Sun</li>
+            <li>Mon</li>
+            <li>Tue</li>
+            <li>Wed</li>
+            <li>Thu</li>
+            <li>Fri</li>
+            <li>Sat</li>
+          </ul>
+          <ul class="days d-flex flex-wrap-wrap margin-top-1-5rem">
+            <li class="inactive">27</li>
+            <li class="inactive">28</li>
+            <li class="inactive">29</li>
+            <li class="inactive">30</li>
+            <li class="inactive">31</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+            <li>11</li>
+            <li>12</li>
+            <li>13</li>
+            <li>14</li>
+            <li>15</li>
+            <li class="active">16</li>
+            <li>17</li>
+            <li>18</li>
+            <li>19</li>
+            <li>20</li>
+            <li>21</li>
+            <li>22</li>
+            <li>23</li>
+            <li>24</li>
+            <li>25</li>
+            <li>26</li>
+            <li>27</li>
+            <li>28</li>
+            <li>29</li>
+            <li>30</li>
+            <li class="inactive">1</li>
+            <li class="inactive">2</li>
+          </ul>
         </div>
       </div>
     </div>
