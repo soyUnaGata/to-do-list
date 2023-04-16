7c6319588c5584cf9e68b6f7ad1076141fdb17b1
soyUnaGata <fallen.snitch@gmail.com>
2023-04-16 11:23:12 +0300

    link icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import main from './assets/css/main.css'
+// import router from './router'
+import './assets/css/main.css'
+import './assets/css/globals.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
