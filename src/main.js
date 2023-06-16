5f45c3bf0e741abd9ef5716ea2919a8dff8ca102
soyUnaGata <fallen.snitch@gmail.com>
2023-06-16 21:46:43 +0300

    add some styles for medai screen mobile 470

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 // import router from './router'
 import './assets/css/main.css'
 import './assets/css/globals.css'
+import './assets/css/media-min-470.css'
 import VueDatePicker from '@vuepic/vue-datepicker';
 import './assets/css/vue-datepicker.css';
 
